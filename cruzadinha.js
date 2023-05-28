45d33903801bbfd5ff0112768f58993982351f02
annamatos <anna.matos@sptech.school>
2023-05-28 13:52:30 -0300

    Id nas palavras, começando validações

--- cruzadinha.js
+++ cruzadinha.js
@@ -1,22 +1,47 @@
+// var palavras = ["m","e","m","o","r","i","a","d","e","m","a","s","s","a"]
+
+// function coferir(){
+//   var letra = "";
+//   var n = 0;
+// for (var contador = 0; contador < palavras.length; contador++) {
+//   n++;
+//   letra = n.value;
+//  if (palavras[contador].includes(letra)){
+// alert(`deu certo caraio`)
+//  }else {
+//   alert(`errou vadia`)
+//  }
+// }}
+
+
 var inputs = document.querySelectorAll("input.inpt");
 
 for (var i = 0; i < inputs.length; i++) {
   inputs[i].addEventListener("input", function() {
     var allValues = "";
-    for (var j = 11; j <= 15; j++) {
+    for (var j = 1; j <= 14; j++) {
       allValues += document.getElementById("" + j).getElementsByTagName("input")[0].value;
     }
-    if (allValues === "GEESE") {
-      for (var k = 11; k <= 15; k++) {
+    if (allValues === "MEMORIADEMASSA") {
+      for (var k = 1; k <= 14; k++) {
         document.getElementById("" + k).getElementsByTagName("input")[0].style.color = "green";
       }
     } else {
-      for (var k = 11; k <= 15; k++) {
+      for (var k = 1; k <= 14; k++) {
         document.getElementById("" + k).getElementsByTagName("input")[0].style.color = "red";
       }
-    }
-  });
-}
+      for (var j = 1; j <= 16; j++) {
+          allValues += document.getElementById("" + j).getElementsByTagName("input")[0].value;
+        }
+        if (allValues === "EPR") {
+          for (var k = 1; k <= 16; k++) {
+            document.getElementById("" + k).getElementsByTagName("input")[0].style.color = "green";
+          }
+        } else {
+          for (var k = 1; k <= 16; k++) {
+            document.getElementById("" + k).getElementsByTagName("input")[0].style.color = "red";
+          }}
+        }})};
 const openModalButton = document.querySelector("#open-modal");
 const closeModalButton = document.querySelector("#close-modal");
 const modal = document.querySelector("#modal");

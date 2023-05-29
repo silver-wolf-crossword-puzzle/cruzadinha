b4e685e9881e59fc3ba965f9038e4e3eccf3f874
annamatos <anna.matos@sptech.school>
2023-05-29 18:44:48 -0300

    validações de letras

--- cruzadinha.js
+++ cruzadinha.js
@@ -1,17 +1,23 @@
-// var palavras = ["m","e","m","o","r","i","a","d","e","m","a","s","s","a"]
 
-// function coferir(){
-//   var letra = "";
-//   var n = 0;
-// for (var contador = 0; contador < palavras.length; contador++) {
-//   n++;
-//   letra = n.value;
-//  if (palavras[contador].includes(letra)){
-// alert(`deu certo caraio`)
-//  }else {
-//   alert(`errou vadia`)
-//  }
-// }}
+// validação
+
+function resultado(input) {
+  var valor = input.value.toLowerCase();
+  var id = input.id;
+
+  if (valor == id) {
+      input.value = `${input.value}`;
+      input.style = `background-color: #a4df66; color:black; border: 1px solid #FFF`;
+  } else {
+      input.style = `background-color: #c51e1c; border: 1px solid #FFF`;
+      input.value = ``;
+  }
+}
+
+
+// fim da validação 
+
+
 var contador = 1
 function proxima() {
   resultado.innerHTML = ``
@@ -101,36 +107,6 @@ function proxima() {
   }
 }
 
-
-
-var inputs = document.querySelectorAll("input.inpt");
-
-for (var i = 0; i < inputs.length; i++) {
-  inputs[i].addEventListener("input", function() {
-    var allValues = "";
-    for (var j = 1; j <= 14; j++) {
-      allValues += document.getElementById("" + j).getElementsByTagName("input")[0].value;
-    }
-    if (allValues === "MEMORIADEMASSA") {
-      for (var k = 1; k <= 14; k++) {
-        document.getElementById("" + k).getElementsByTagName("input")[0].style.color = "green";
-      }
-    } else {
-      for (var k = 1; k <= 14; k++) {
-        document.getElementById("" + k).getElementsByTagName("input")[0].style.color = "red";
-      }
-      for (var j = 1; j <= 16; j++) {
-          allValues += document.getElementById("" + j).getElementsByTagName("input")[0].value;
-        }
-        if (allValues === "EPR") {
-          for (var k = 1; k <= 16; k++) {
-            document.getElementById("" + k).getElementsByTagName("input")[0].style.color = "green";
-          }
-        } else {
-          for (var k = 1; k <= 16; k++) {
-            document.getElementById("" + k).getElementsByTagName("input")[0].style.color = "red";
-          }}
-        }})};
 const openModalButton = document.querySelector("#open-modal");
 const closeModalButton = document.querySelector("#close-modal");
 const modal = document.querySelector("#modal");
